b22d0a97f1c374d024d8f6632c8ea0b39ac529ea
anfeichtinger <anfeichtingers@gmail.com>
2021-04-18 19:27:44 +0200

    complete refactor and exchange, new project with wpf and c#

---
+++ OpenTrainerProject/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Memory;
+
+namespace OpenTrainerProject
+{
+
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                DragMove();
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        public Mem MemLib = new Mem();
+        bool gameProc = false;
+        private void ProceedButton_Click(object sender, RoutedEventArgs e)
+        {
+            gameProc = MemLib.OpenProcess("explorer");
+            if (gameProc)
+            {
+                WindowTitle.Text = "Found Explorer.exe";
+                MemLib.OpenProcess("explorer");
+            }
+            else 
+            {
+                WindowTitle.Text = "Open Trainer Project";
+            }
+        }
+    }
+}
