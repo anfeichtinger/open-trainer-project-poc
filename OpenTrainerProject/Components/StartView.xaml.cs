d4ed1bd2ebdcc7e425f3a91a1a9fb14508ce8f74
anfeichtinger <anfeichtingers@gmail.com>
2021-04-22 12:46:21 +0200

    prepare api communication

--- OpenTrainerProject/Components/StartView.xaml.cs
+++ OpenTrainerProject/Components/StartView.xaml.cs
@@ -1,42 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OpenTrainerProject.Components
 {
-    /// <summary>
-    /// Interaction logic for StartView.xaml
-    /// </summary>
     public partial class StartView : UserControl
     {
         public StartView()
         {
             InitializeComponent();
+
+            // Fetch Trainers from API in background thread
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += fetchTrainers;
+            worker.RunWorkerCompleted += fetchTrainersCompleted;
+            worker.RunWorkerAsync();
         }
+
+        private void fetchTrainers(object sender, DoWorkEventArgs e)
+        {
+            //
+        }
+        private void fetchTrainersCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //
+        }
+        // Change window content
         private void ProceedButton_Click(object sender, RoutedEventArgs e)
         {
             MainWindow window = (MainWindow)Window.GetWindow(this);
             window.WindowContent = new TrainerView();
         }
-
+        // Open Github Repo in Browser
         private void GithubButton_Click(object sender, RoutedEventArgs e)
         {
             System.Diagnostics.Process.Start("https://github.com/anfeichtinger/open-trainer-project");
         }
+        // Open Donation Link in Browser
         private void DonateButton_Click(object sender, RoutedEventArgs e)
         {
             System.Diagnostics.Process.Start("https://www.paypal.com/donate/?hosted_button_id=EE3W7PS6AHEP8");
+        }
+
+        private void GameComboBox_DropDownClosed(object sender, EventArgs e)
+        {
+            //
         }
     }
 }
--- OpenTrainerProject/Components/TrainerView.xaml.cs
+++ OpenTrainerProject/Components/TrainerView.xaml.cs
@@ -1,54 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Memory;
 
 namespace OpenTrainerProject.Components
 {
-    /// <summary>
-    /// Interaction logic for TrainerView.xaml
-    /// </summary>
     public partial class TrainerView : UserControl
     {
         public TrainerView()
         {
             InitializeComponent();
         }
-        public Mem MemLib = new Mem();
-        bool gameProc = false;
 
         private void ProceedButton_Click(object sender, RoutedEventArgs e)
         {
-            gameProc = MemLib.OpenProcess("explorer");
-            if (gameProc)
-            {
-                TitleBar.TitleText = "Found Explorer.exe";
-                MemLib.OpenProcess("explorer");
-            }
-            else
-            {
-                TitleBar.TitleText = "Game Not Found";
-            }
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += fetchTrainer;
+            worker.RunWorkerCompleted += fetchTrainerCompleted;
+            worker.RunWorkerAsync();
+            Mouse.OverrideCursor = Cursors.Wait;
         }
-
-        private void GithubButton_Click(object sender, RoutedEventArgs e)
+        private void fetchTrainer(object sender, DoWorkEventArgs e)
         {
-            System.Diagnostics.Process.Start("https://github.com/anfeichtinger/open-trainer-project");
+            Thread.Sleep(2000);
         }
-        private void DonateButton_Click(object sender, RoutedEventArgs e)
+        private void fetchTrainerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            System.Diagnostics.Process.Start("https://www.paypal.com/donate/?hosted_button_id=EE3W7PS6AHEP8");
+            Mouse.OverrideCursor = Cursors.Arrow;
         }
     }
 }
